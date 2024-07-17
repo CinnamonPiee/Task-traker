3018e98393162ed543e65f6912b189407902c5ab
CinnamonPie <vonahkahs.s.ved@gmail.com>
2024-07-17 10:16:47 +0400

    create tasts and change models in db

--- src/auth/models.py
+++ src/auth/models.py
@@ -1,20 +1,27 @@
 from datetime import datetime
-from sqlalchemy import ForeignKey, String, Table, Integer, Boolean, MetaData, Column, TIMESTAMP
+
 from fastapi_users_db_sqlalchemy import SQLAlchemyBaseUserTable
-from database import Base
+from sqlalchemy import (JSON, TIMESTAMP, Boolean, Column, ForeignKey, Integer,
+                        String, Table)
 
+from src.database import Base, metadata
 
-metadata = MetaData()
-
+role = Table(
+    "role",
+    metadata,
+    Column("id", Integer, primary_key=True),
+    Column("name", String, nullable=False),
+    Column("permissions", JSON),
+)
 
 user = Table(
     "user",
     metadata,
     Column("id", Integer, primary_key=True),
-    Column("name", String, nullable=False),
     Column("email", String, nullable=False),
     Column("username", String, nullable=False),
     Column("registered_at", TIMESTAMP, default=datetime.utcnow),
+    Column("role_id", Integer, ForeignKey(role.c.id)),
     Column("hashed_password", String, nullable=False),
     Column("is_active", Boolean, default=True, nullable=False),
     Column("is_superuser", Boolean, default=False, nullable=False),
@@ -22,22 +29,12 @@ user = Table(
 )
 
 
-task = Table(
-    "task",
-    metadata,
-    Column("id", Integer, primary_key=True),
-    Column("name", String, nullable=False),
-    Column("user_id", Integer, ForeignKey(user.c.id)),
-    Column("created_at", TIMESTAMP, default=datetime.utcnow),
-)
-
-
 class User(SQLAlchemyBaseUserTable[int], Base):
     id = Column(Integer, primary_key=True)
-    name = Column(String, nullable=False)
     email = Column(String, nullable=False)
     username = Column(String, nullable=False)
     registered_at = Column(TIMESTAMP, default=datetime.utcnow)
+    role_id = Column(Integer, ForeignKey(role.c.id))
     hashed_password: str = Column(String(length=1024), nullable=False)
     is_active: bool = Column(Boolean, default=True, nullable=False)
     is_superuser: bool = Column(Boolean, default=False, nullable=False)
